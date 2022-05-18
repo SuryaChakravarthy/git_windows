e8765f716fafc5f19842c6be6dc15d32b419f2a6
chakri <surya.chakravarthi@wisseninfotech.com>
2022-05-18 10:33:11 +0530

    first commit of windows

---
+++ cdn.ps1
@@ -0,0 +1,27 @@
+﻿Connect-AzAccount
+
+Get-AzResourceGroup
+
+$ResourceGroup="Azure_L1_Batch_7"
+
+$StorageAccount=New-AzStorageAccount -ResourceGroupName $ResourceGroup -Name "suryapec" -SkuName Standard_LRS -Location Eastus -Kind Storage
+
+$Context = $StorageAccount.Context
+
+New-AzStorageContainer -Name suryaproject -Permission Blob -Context $Context
+
+
+New-AzCdnProfile -ProfileName project1 -ResourceGroupName $ResourceGroup -Sku Standard_Microsoft -Location "East US"
+
+New-AzCdnEndpoint -ProfileName project1 -ResourceGroupName $ResourceGroup -Location "East US" -EndpointName suryapeccdn -OriginName "suryapec" -OriginHostName "suryapec.blob.core.windows.net"
+
+
+
+
+
+
+
+
+
+
+
